78f28ad971d16748aead4af6120ba0ada991d801
KiranKumarDevapatla <devapatlakirankumar@gmail.com>
2024-04-05 14:33:19 +0530

    added env file

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,6 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
+
 module.exports = {
-  key: "5d2a4d30151981dfc7cdeec229c386b6",
-  base: "https://api.openweathermap.org/data/2.5/",
+  key: process.env.REACT_APP_WEATHER_API_KEY,
+  base: process.env.REACT_APP_WEATHER_API_BASE,
 };
