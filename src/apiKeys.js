ff331563c9d850cd49816ed5c7d24e8fabc932a5
KiranKumarDevapatla <devapatlakirankumar@gmail.com>
2024-04-05 11:40:18 +0530

    changed node version

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "5d2a4d30151981dfc7cdeec229c386b6", // Remove curly braces
+  key: "5d2a4d30151981dfc7cdeec229c386b6",
   base: "https://api.openweathermap.org/data/2.5/",
 };
